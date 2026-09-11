097f00d5c8f0a3f95b2fcd434a43fad31f3404d5
Newmi-Git <yaghyaabdul1006@gmail.com>
2026-09-11 14:48:36 +0200

    Added a proper test for the AI

--- mnist_test.py
+++ mnist_test.py
@@ -4,17 +4,19 @@ from torchvision.transforms import ToTensor
 import matplotlib.pyplot as plt
 import torch.nn as nn
 from torch.utils.data import DataLoader
+import torch.optim as optim
+
 
 class ImageRecog(nn.Module):
     def __init__(self):
         super().__init__()
         
         self.network = nn.Sequential(
-            nn.Linear(786, 128),
-            nn.ReLU,
+            nn.Linear(784, 128),
+            nn.ReLU(),
             
             nn.Linear(128, 64),
-            nn.ReLU,
+            nn.ReLU(),
             
             nn.Linear(64, 10)
         )
@@ -24,10 +26,13 @@ class ImageRecog(nn.Module):
     
 model = ImageRecog()
 
+loss_function = nn.CrossEntropyLoss()
 
+optimizer = optim.Adam(
+    model.parameters(),
+    lr=0.001
+)
     
-    
-
 dataset = datasets.MNIST(
     root="datasets",
     train=True,
@@ -41,18 +46,89 @@ train_loader = DataLoader(
     shuffle=True
 )
 
+
+test_dataset = datasets.MNIST(
+    root="datasets",
+    train=False,
+    download=True,
+    transform=ToTensor()    
+)
+
+test_loader = DataLoader(
+    test_dataset,
+    batch_size=64,
+    shuffle=False
+)
+
 image, label = dataset[0]
-
-print("Image Shape: ", image.shape)
-flattened = image.flatten()
+images, labels = next(iter(train_loader))
 
 
+for epoch in range(5):
+    total_loss = 0
+    for images, labels in train_loader:
+        images = images.view(images.size(0), -1)
+        prediction = model(images)
+        
+        loss = loss_function(prediction, labels) #how
+        optimizer.zero_grad() #Which
+        loss.backward() #How
 
-plt.imshow(image.squeeze(),cmap="gray")
-plt.title(f"Digit: {label}")
-plt.axis("off")
-plt.show()
+        optimizer.step() #Do
+        
+        total_loss += loss.item()
+        
+    average_loss = total_loss / len(train_loader)
+    
+    print(f"Epoch: {epoch + 1}/5 Total loss: {total_loss}, Average loss: {average_loss}")
 
 
+correct = 0
+total = 0
 
+model.eval()
+
+with torch.no_grad():
+    for images, labels in test_loader:
+        images = images.view(images.size(0), -1)
+        prediction = model(images)
+        
+        predicted_digits=prediction.argmax(dim=1)
+        
+        correct += (predicted_digits == labels).sum().item()
+        total += labels.size(0)
+        
+    accuracy = correct / total * 100
+    print(f"Test accuracy: ", accuracy)
+
+
+model.eval()
+
+images, labels = next(iter(test_loader))
+
+original_images = images
+
+images = images.view(images.size(0), -1)
+
+with torch.no_grad():
+    predictions = model(images)
+    
+predicted_digits = predictions.argmax(dim=-1)
+
+for i in range(10):
+    
+    plt.figure()
+    
+    plt.imshow(original_images[i].squeeze(), cmap="gray")
+    
+    actual = labels[i].item()
+    predicted = predicted_digits[i].item()
+    
+    plt.title(f"Actual: {actual}\n Predicted: {predicted}")
+    plt.show()
+
+'''
+for epoch in range(1000):
+    prediction(model())
+'''
 
